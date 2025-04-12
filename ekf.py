6b3e2714941e396679eff9ff99afbaf60dac09f3
celik02 <emrullah.celik0202@gmail.com>
2025-04-12 19:49:55 -0400

    added vehicle class

--- ekf.py
+++ ekf.py
@@ -1,141 +1,281 @@
-import filterpy
 import numpy as np
 from filterpy.kalman import ExtendedKalmanFilter
 from carla_setup import CarlaManager
+from vehicle import Vehicle
+import copy
+import time
+import threading
+from numpy import dot
 state_dim = 5  # State dimension
 measurement_dim = 2  # Measurement dimension
 control_dim = 2  # Control dimension
 
 
-def vehicle_dynamics(x, dt, u):
-    """
-    # TODO we should standardize the vehicle dynamics model across all files
-    Bicycle kinematic model with longitudinal dynamics
+class CustomEKF(ExtendedKalmanFilter):
+    def __init__(self, dim_x, dim_z, dim_u):
+        super().__init__(dim_x=dim_x, dim_z=dim_z)
+        self.dim_u = dim_u  # Control dimension
+        self.u = None  # Control input
 
-    States:
-        x[0]: x position
-        x[1]: y position
-        x[2]: heading angle (psi)
-        x[3]: velocity
-        x[4]: acceleration
+    def predict_x(self, u=0,  dt=0.1):
+        if u is not None:
+            self.u = u
+        # F updated with the current state estimate and control input
+        print('inside custom predict_x with u:', u)
+        self.F = self.F_jacobian(self.x, dt, self.u)
+        self.x = self.vehicle_dynamics(self.x, dt, self.u)  # state prediction
+        print('Predicting state:', self.x)
 
-    Controls:
-        u[0]: acceleration command
-        u[1]: steering angle
-    """
-    # Vehicle parameters
-    L = 3  # Wheelbase
-    tau_a = 0.5  # Acceleration time constant
-    a = u[0]  # Acceleration command
-    delta = u[1]  # Steering angle
-    # State update
-    x_next = np.zeros_like(x)
-    x_next[0] = x[0] + x[3] * np.cos(x[2]) * dt
-    x_next[1] = x[1] + x[3] * np.sin(x[2]) * dt
-    x_next[2] = x[2] + x[3] * np.tan(delta) / L * dt
-    x_next[3] = x[3] + x[4] * dt
-    x_next[4] = x[4] + (a - x[4]) / tau_a * dt
+    def predict_x_imu(self, imu_data=None, dt=0.01):
+        """
+        Predict the state using IMU data.
+        :param imu_data: IMU data to use for prediction
+        :param dt: Time step for prediction
+        """
+        if imu_data is None:
+            return
+        # IMU reference frame aligned with the vehicle's coordinate system
+        # I need ax for forward acceleration,
+        # gz for angular rate around z-axis (yaw rate)
+        ax, ay, az, gx, gy, gz = imu_data
+        u = [ax, gz]
+        # Use the IMU data to predict the state
+        self.F = self.F_jacobian(self.x, dt, u=u, imu_prediction=True)
+        self.x = self.vehicle_dynamics(self.x, dt, u=u, imu_prediction=True)  # state prediction
 
-    return x_next
+        # self.P = dot(self.F, self.P).dot(self.F.T) + self.Q  # this is prior
+        print('Predicting with IMU data:', self.x)
 
+    def predict(self, u=None):
+        self.predict_x(u)
+        self.P = dot(self.F, self.P).dot(self.F.T) + self.Q  # this is prior
 
-def F_jacobian(x, dt, u):
-    """
-    Jacobian of the vehicle dynamics model
-    """
-    L = 3  # Wheelbase
-    tau_a = 0.5  # Acceleration time constant
-    a = u[0]  # Acceleration command
-    delta = u[1]  # Steering angle
-    p_x, p_y, psi, v, a_curr = x
-    # State Jacobian
-    F = np.zeros((5, 5))
+        # save prior
+        self.x_prior = np.copy(self.x)
+        self.P_prior = np.copy(self.P)
 
-    F[0, 0] = 1
-    F[0, 2] = -v * np.sin(psi) * dt
-    F[0, 3] = np.cos(psi) * dt
+    def update(self, z, R=None, residual=np.subtract):
+        print('inside update:', z)
+        super().update(z, HJacobian=self.HJacobian_GPS, Hx=self.Hx, R=R,
+                       residual=residual)
+        self.u = None  # Reset control input after update
 
-    F[1, 1] = 1
-    F[1, 2] = v * np.cos(psi) * dt
-    F[1, 3] = np.sin(psi) * dt
+    def vehicle_dynamics(self, x, dt, u, imu_prediction=False):
+        """
+        # TODO we should standardize the vehicle dynamics model across all files
+        Bicycle kinematic model with longitudinal dynamics
 
-    F[2, 2] = 1
-    F[2, 3] = (np.tan(delta) / L) * dt
+        States:
+            x[0]: x position
+            x[1]: y position
+            x[2]: heading angle (psi)
+            x[3]: velocity
+            x[4]: acceleration
 
-    F[3, 3] = 1
-    F[3, 4] = dt
+        Controls:
+            u[0]: acceleration command
+            u[1]: steering angle
+        """
+        # Vehicle parameters
+        L = 3  # Wheelbase
+        tau_a = 0.5  # Acceleration time constant
+        a = u[0]  # Acceleration command
+        delta = u[1]  # Steering angle
+        # State update
+        x_next = np.zeros_like(x)
+        x_next[0] = x[0] + x[3] * np.cos(x[2]) * dt
+        x_next[1] = x[1] + x[3] * np.sin(x[2]) * dt
+        if imu_prediction:
+            # If using IMU prediction, we can skip the heading update from control input
+            # This is a placeholder for IMU prediction
+            x_next[2] = x[2] + u[1] * dt
+        else:
+            x_next[2] = x[2] + x[3] * np.tan(delta) / L * dt
+        x_next[3] = x[3] + x[4] * dt
+        x_next[4] = x[4] + (a - x[4]) / tau_a * dt
+        print('xnext:', x_next)
+        return x_next
 
-    F[4, 4] = 1 - dt / tau_a
+    @staticmethod
+    def F_jacobian(x, dt, u, imu_prediction=False):
+        """
+        Jacobian of the vehicle dynamics model
+        """
+        L = 3  # Wheelbase
+        tau_a = 0.5  # Acceleration time constant
+        a = u[0]  # Acceleration command
+        delta = u[1]  # Steering angle
+        p_x, p_y, psi, v, a_curr = x
+        # State Jacobian
+        F = np.zeros((5, 5))
 
-    return F
+        F[0, 0] = 1
+        F[0, 2] = -v * np.sin(psi) * dt
+        F[0, 3] = np.cos(psi) * dt
 
+        F[1, 1] = 1
+        F[1, 2] = v * np.cos(psi) * dt
+        F[1, 3] = np.sin(psi) * dt
 
-def hx(x):
-    """
-    Measurement function that maps the state to the measurement space.
-    Here, we assume the GPS provides position measurements [x, y].
-    """
-    return np.array([x[0], x[1]])
+        F[2, 2] = 1
 
+        if imu_prediction:
+            # If using IMU prediction, we can skip the heading update from control input
+            F[2, 3] = 0
+        else:
+            F[2, 3] = (np.tan(delta) / L) * dt
 
-def H_jacobian(x):
-    """
-    Jacobian of the measurement function with respect to the state.
-    Since our measurement is simply the position, the Jacobian is:
+        F[3, 3] = 1
+        F[3, 4] = dt
 
-    H = [1  0  0  0
-        0  1  0  0]
-    """
-    H = np.zeros((2, 4))
-    H[0, 0] = 1.0
-    H[1, 1] = 1.0
-    return H
+        F[4, 4] = 1 - dt / tau_a
+
+        return F
+
+    @staticmethod
+    def Hx(x):
+        """
+        Measurement function that maps the state to the measurement space.
+        Here, we assume the GPS provides position measurements [x, y].
+        """
+        return np.array([x[0], x[1]])
+
+    @staticmethod
+    def HJacobian_GPS(x):
+        """ for GPS measurement
+        Jacobian of the measurement function with respect to the state.
+        Since our measurement is simply the position, the Jacobian is:
+
+        H = [1  0  0  0 0
+            0  1  0  0 0]
+        """
+        H = np.zeros((2, 5))
+        H[0, 0] = 1.0
+        H[1, 1] = 1.0
+        return H
+
+    # # measurement jacobian for IMU sensor
+    # @staticmethod
+    # def HJacobian_IMU(x):
+    #     """ for IMU measurement
+    #     Jacobian of the measurement function with respect to the state.
+    #     Since our measurement is simply the position, the Jacobian is:
+
+    #     H = [1  0  0  0 0
+    #         0  1  0  0 0]
+    #     """
+    #     # TODO inspect the IMU measurement
+    #     H = np.zeros((2, 5))
+    #     H[0, 3] = x[3] * np.cos(x[2])
+    #     H[1, 4] = x[4] * np.sin(x[2])
+    #     return H
 
 
 def set_extended_kalman_filter(x0, P0, Q, R):
     """
     Initialize the Extended Kalman Filter with the given parameters.
     """
-    ekf = ExtendedKalmanFilter(dim_x=state_dim, dim_z=measurement_dim, dim_u=control_dim)
+    ekf = CustomEKF(dim_x=state_dim, dim_z=measurement_dim, dim_u=control_dim)
     ekf.x = x0  # Initial state
     ekf.P = P0  # Initial covariance
     ekf.Q = Q  # Process noise covariance
     ekf.R = R  # Measurement noise covariance
-    ekf.F = F_jacobian  # State transition function
-    ekf.H = H_jacobian  # Measurement function
-    ekf.hx = hx  # Measurement function
+
+    # ekf.F_jacobian = lambda x, dt, u: F_jacobian(x, dt, u)
+
+    # ekf.H_jacobian = HJacobian  # Measurement function
+    # ekf.hx = hx  # Measurement function
+    # ekf.B = None
     return ekf
 
 
-x0 = np.array([0, 0, 0, 0, 0])  # Initial state
-P0 = np.array([10., 10., 10., 10, 10.])  # Initial covariance
-Q = np.eye(state_dim) * 0.1  # Process noise covariance
-R = np.eye(measurement_dim) * 1  # Measurement noise covariance
+def ekf_update_loop(vehicle):
+    """
+    A loop to continuously update the EKF for a vehicle.
+    :param vehicle: The vehicle object containing the EKF instance.
+    """
+    while True:
+        update_interval = 0.1  # 20 Hz
+        while True:
+            vehicle.update_ekf()
+            time.sleep(update_interval)
+
+
+x0 = np.array([0.1, 0.1, 0.1, 0.1, 0.1])  # Initial state
+P0 = np.array([100., 100., 1., 1, 1.])  # Initial covariance
+Q = np.eye(state_dim) * 0.5  # Process noise covariance
+R = np.eye(measurement_dim) * 3  # Measurement noise covariance
+
+SPAWN_LOCATION = [
+    111.229362,
+    13.511219,
+    14.171306,
+]  # for spectator camera
 
 
 if __name__ == "__main__":
-    ekf = set_extended_kalman_filter(x0, P0, Q, R)
+    ekf: CustomEKF = set_extended_kalman_filter(x0, P0, Q, R)
     print("Initial state:", ekf.x)
     print("Initial covariance:", ekf.P)
     print("Process noise covariance:", ekf.Q)
     print("Measurement noise covariance:", ekf.R)
+
     carla_manager = CarlaManager()
-    vehicle = carla_manager.spawn_vehicle("vehicle.tesla.model3", [0, 0, 0])
-    vehicle.set_autopilot(False)
-    carla_manager.set_vehicle_control(vehicle, throttle=0.5, steer=0.1)
+    ego, imu, gps = carla_manager.spawn_vehicle("vehicle.tesla.model3", SPAWN_LOCATION, GPS=True, IMU=True)
+    time.sleep(2)  # Allow some time for the vehicle to initialize
+    ego.set_autopilot(True)
+    ego_vehicle = Vehicle(ego, copy.deepcopy(ekf))
+    ego_vehicle.attach_sensors(GPS=gps, IMU=imu)
 
     # Simulate vehicle dynamics
     dt = 0.1  # Time step
-    u = np.array([0.5, 0.1])  # Control input (acceleration, steering angle)
-    
-    for _ in range(10):
+    # u = np.array([0, 0.0])  # Control input (acceleration, steering angle)
+
+    # plot the results
+    import matplotlib.pyplot as plt
+    plt.ion()  # Turn on interactive mode
+    plt.figure()
+    plt.title("EKF State Estimation")
+    plt.xlabel("X position")
+    plt.ylabel("Y position")
+    plt.grid()
+    # print(ekf)
+    ekf_thread = threading.Thread(target=ekf_update_loop, args=(ego_vehicle,), daemon=True)
+    ekf_thread.start()
+    while True:
         # Simulate vehicle dynamics
-        ekf.predict(u=u, dt=dt)
-        print("Predicted state:", ekf.x)
-        print("Predicted covariance:", ekf.P)
+        # ekf.predict(u=u)
 
         # Simulate GPS measurement
-        z = np.array([ekf.x[0] + np.random.normal(0, 1), ekf.x[1] + np.random.normal(0, 1)])
-        ekf.update(z)
-        print("Updated state:", ekf.x)
-        print("Updated covariance:", ekf.P)
+        # z = np.array([ekf.x[0] + np.random.normal(0, 1) + u[0], ekf.x[1] + np.random.normal(0, 1) + u[1]])
+        # ekf.update(z)
+        control = ego_vehicle.actor.get_control()
+        print(control)  # Ensure the actor is in control mode
+        u = np.array([1.5, 0.5])
+        # Plot the current state
+        ekf_state_x = ego_vehicle.get_latest_state()
+
+        print('frenet states:', ego_vehicle.get_frenet_states())
+
+        plt.scatter(ekf_state_x[0], ekf_state_x[1], color='r', marker='x', label='EKF Estimate')  # Plot EKF estimated position
+        plt.scatter(ego_vehicle.actor.get_transform().location.x,
+                    ego_vehicle.actor.get_transform().location.y,
+                    color='b', label='Actual Position')
+
+        # plot gps location
+        plt.scatter(ego_vehicle.carla_coords[0], ego_vehicle.carla_coords[1], color='g', label='GPS Location')
+
+        ego_vehicle.ekf.predict(u=u)
+        # ego_vehicle.update_ekf()
+
+        # update spectator camera to follow the vehicle
+        spectator_location = ego_vehicle.actor.get_transform()
+        spectator_location.location.z += 5
+        spectator_location.location.x += 5
+        carla_manager.spectator.set_transform(spectator_location)  # Keep the spectator camera in the same location
+
+        # update the world
+        carla_manager.world.tick()
+        plt.pause(0.5)
+    ekf_thread.join()  # Ensure the EKF update loop thread is joined before exiting
+    carla_manager.__del__()  # Clean up the Carla manager and close the simulation
---
+++ vehicle.py
@@ -0,0 +1,302 @@
+# from ekf import CustomEKF
+import numpy as np
+import threading
+import queue
+import copy
+import traceback
+from scipy.optimize import least_squares
+import carla
+import math
+
+
+class Vehicle:
+    """
+    Vehicle class to represent a vehicle in the simulation.
+    """
+
+    def __init__(self, actor, ekf=None):
+        """
+        Initialize the vehicle with a Carla actor.
+        :param actor: The Carla actor representing the vehicle.
+        """
+        self.actor = actor
+        self.ekf = ekf
+        self.imu_data_queue = queue.Queue(maxsize=10)
+        self.gps_data_queue = queue.Queue(maxsize=5)
+        self.state_lock = threading.Lock()
+        self.latest_state = np.zeros(5)  # Initialize latest state to zero, will be updated by EKF]
+        self.wheel_base = 2.5  # Example wheel base, adjust as necessary  # FIXME
+        # TODO set initial state of EKF
+        # self.ekf.x = np.array([0, 0, 0, 0, 0])  # Example initial state
+        self.carla_coords = np.array([0.0, 0.0, 0.0])  # Placeholder for GPS coordinates
+
+    def attach_sensors(self, GPS=None, IMU=None):
+        """
+        Attach sensors to the vehicle.
+        :param GPS: Optional GPS sensor to attach.
+        :param IMU: Optional IMU sensor to attach.
+        """
+        self.gps = GPS
+        self.imu = IMU
+
+        self.gps.listen(self.gps_callback) if GPS else None
+        self.imu.listen(self.imu_callback) if IMU else None
+
+    def gps_callback(self, data):
+        """
+        Callback function for GPS data.
+        :param data: The GPS data received from the sensor.
+        """
+        print("Received GPS data callback")  # Debug statement to confirm callback is called
+        self.gps_data = data
+        self.gps_arr = copy.copy(np.array([self.gps_data.latitude, self.gps_data.longitude, self.gps_data.altitude]))
+        (carla_x, carla_y, carla_z) = self.convert_gps_to_carla(self.gps_arr)
+        self.carla_coords = (carla_x, carla_y, carla_z)
+        try:
+            self.gps_data_queue.put_nowait(copy.deepcopy(self.carla_coords))
+        except queue.Full:
+            # print("GPS queue full, dropping data.")
+            pass
+
+        # print('Vehicle actual position:', self.actor.get_transform().location)
+
+    def convert_gps_to_carla(self, gps):
+        """
+        Converts GPS signal into the CARLA coordinate frame
+        :param gps: gps from gnss sensor
+        :return: gps as numpy array in CARLA coordinates
+        """
+        mean = np.array([0, 0, 0])
+        scale = np.array([111319.49082349832, 111319.49079327358, 1.0])
+        gps = (gps - mean) * scale
+        # GPS uses a different coordinate system than CARLA.
+        # This converts from GPS -> CARLA (90° rotation)
+        gps = np.array([gps[1], -gps[0], gps[2]])
+        return gps
+
+    def imu_callback(self, data):
+        """
+        Callback function for IMU data.
+        :param data: The IMU data received from the sensor.
+        """
+        # TODO if necessary data could be put into thread safe queue
+        # Extract IMU data
+        self.imu_data = data
+        self.imu_data_array = np.array([-self.imu_data.accelerometer.x, self.imu_data.accelerometer.y, self.imu_data.accelerometer.z,
+                                        self.imu_data.gyroscope.x, self.imu_data.gyroscope.y, self.imu_data.gyroscope.z])
+        # print(f"IMU data received: {self.imu_data_array}")
+        try:
+            self.imu_data_queue.put_nowait(copy.deepcopy(self.imu_data_array))
+        except queue.Full:
+            # print("IMU queue full, dropping data.")
+            pass
+
+    def apply_control(self, control):
+        """
+        Apply control to the vehicle.
+        :param control: The control to apply (e.g., throttle, brake, steer).
+        """
+        self.actor.apply_control(control)
+
+    def update_ekf(self):
+        """
+        Update the Extended Kalman Filter (EKF) if it is set.
+        This function can be called to update the EKF with the latest sensor data.
+        """
+        # try:
+        #     imu_data = self.imu_data_queue.get_nowait()
+        #     print(f"Retrieved IMU data for EKF update: {imu_data}")
+        #     self.ekf.predict_x_imu(imu_data)  # FIXME # Perform the prediction step before update
+        # except queue.Empty:
+        #     print("No IMU data available for EKF update.")
+        #     imu_data = None
+        # except Exception as e:
+        #     print('ERROR', e)
+        #     traceback.print_exc()
+
+        # try:
+        #     gps_data = self.gps_data_queue.get_nowait()
+        #     z = np.array([gps_data[0], gps_data[1]])
+        #     self.ekf.update(z)
+        #     print(f"Updating EKF with GPS data: {gps_data}")
+        # except queue.Empty:
+        #     print("No GPS data available for EKF update.")
+        #     gps_data = None
+        # except Exception as e:
+        #     print('ERROR', e)
+        #     traceback.print_exc()
+
+        with self.state_lock:
+            self.latest_state = self.ekf.x.copy()
+
+    def get_latest_state(self):
+        with self.state_lock:
+            return self.latest_state
+
+    def get_frenet_states(self, next_waypoint):
+        """
+        Get the Frenet states of the vehicle.
+        :return: The Frenet coordinates as a numpy array [s, d, mu, speed, steering_angle, curvature]
+        """
+        velocity = self.actor.get_velocity()
+        speed = np.sqrt(velocity.x**2 + velocity.y**2 + velocity.z**2)  # speed in m/s
+
+        # Get vehicle control (steering)
+        control = self.actor.get_control()
+        steering_angle = np.clip(control.steer, -1, 1)
+
+        # calculate heading error
+        self.waypoint = next_waypoint
+
+        # # Approximate curvature (kappa) based on steering angle and vehicle wheel base
+        # wheel_base = 2.5  # You may need to adjust this based on the vehicle
+        # curvature = steering_angle / wheel_base
+
+        # if we need curvature, we need to calculate it --> we need next 15 or so waypoints
+        # _, _, r_fit = fit_circle(self.path_to_follow_waypoints, next_waypoint)
+
+        try:
+            # self.curvature_speed = 1 / r_fit
+            curvature = 0
+        except FloatingPointError as e:
+            curvature = 0
+            print(f"Caught a floating point error: {e}")
+        except Exception as e:
+            print(f"Caught an exception: {e}")
+            traceback.print_exc()
+
+        self.target_heading = np.radians(self.waypoint.transform.rotation.yaw) % (
+            2 * np.pi
+        )
+
+        mu = (
+            np.radians(
+                self.waypoint.transform.rotation.yaw
+                - (
+                    self.vehicle.get_transform().rotation.yaw
+                    + np.arctan(steering_angle / self.wheel_base)
+                )
+            )
+            + np.pi
+        ) % (2 * np.pi) - np.pi
+
+        self.s += self.calculate_progress()
+
+        d = self.calculate_deviation(self, self.waypoint)
+        state_data = np.array([self.s, d, -mu, speed, steering_angle, curvature])
+
+        return state_data, self.waypoint
+
+    def calculate_deviation(self, waypoint):
+        """
+        Calculate the signed deviation of the vehicle from the center of the lane.
+        """
+        # Get the current location and heading of the vehicle
+        vehicle_location = self.actor.get_location()
+
+        # Get the map and the waypoint at the vehicle's location
+
+        # Get the lane center and lane direction
+        lane_transform = waypoint.transform
+        lane_center_location = lane_transform.location
+        lane_heading = lane_transform.rotation.yaw
+        lane_heading_rad = math.radians(lane_heading)
+
+        # Calculate the direction vector of the lane
+        lane_direction = carla.Vector3D(
+            math.cos(lane_heading_rad), math.sin(lane_heading_rad), 0
+        )
+
+        # Calculate the relative position vector from the lane center to the vehicle
+        vehicle_vector = carla.Vector3D(
+            vehicle_location.x - lane_center_location.x,
+            vehicle_location.y - lane_center_location.y,
+            0,
+        )
+
+        # Calculate the cross product (z-component) to determine the side
+        cross_product_z = (
+            lane_direction.x * vehicle_vector.y - lane_direction.y * vehicle_vector.x
+        )
+
+        # Calculate the Euclidean distance for the deviation
+        deviation = math.sqrt(vehicle_vector.x**2 + vehicle_vector.y**2)
+
+        # Apply the sign based on the cross product
+        if cross_product_z < 0:
+            deviation = -deviation
+
+        return deviation
+
+    def calculate_progress(self):
+        """
+        Calculate the progress 's' along the lane for a CARLA vehicle.
+        Returns:
+        - s (float): The progress along the lane.
+        """
+        # Get the vehicle's transform
+        vehicle_transform = self.vehicle.get_transform()
+        vehicle_location = vehicle_transform.location
+        s = vehicle_location.distance(self.prev_loc)
+        self.prev_loc = vehicle_location
+        return s
+
+
+# if we need curvature we will need this function
+def fit_circle(path_to_follow_waypoints, next_waypoint):
+    # Initial guess for parameters: center at mean of points, radius as half of maximum distance from center
+    x_centerline = []
+    y_centerline = []
+    index = path_to_follow_waypoints.index(next_waypoint)
+    waypoint = next_waypoint[0]
+    # for _ in range(0, 20):
+    #     x_centerline.append(waypoint.transform.location.x)
+    #     y_centerline.append(waypoint.transform.location.y)
+    #     waypoint = waypoint.next(.5)[0]
+    for i in range(0, 15):
+        if i + index >= len(path_to_follow_waypoints):
+            break
+        x_centerline.append(path_to_follow_waypoints[index + i][0].transform.location.x)
+        y_centerline.append(path_to_follow_waypoints[index + i][0].transform.location.y)
+
+    points = np.column_stack((x_centerline, y_centerline))
+    xc_initial = np.mean(points[:, 0])
+    yc_initial = np.mean(points[:, 1])
+    r_initial = (
+        np.max(
+            np.sqrt((points[:, 0] - xc_initial) ** 2 + (points[:, 1] - yc_initial) ** 2)
+        )
+        / 2
+    )
+
+    params_initial = [xc_initial, yc_initial, r_initial]
+
+    result = least_squares(circle_residuals, params_initial, args=(points,))
+    xc, yc, r = result.x
+
+    first_point = points[0]
+    tangent_vector = np.array(
+        [
+            waypoint.transform.rotation.get_forward_vector().x,
+            waypoint.transform.rotation.get_forward_vector().y,
+        ]
+    )
+
+    # Calculate vector from circle center to the first point
+    center_to_point_vector = first_point - np.array([xc, yc])
+
+    # Calculate the cross product
+    cross_product = np.cross(
+        np.append(tangent_vector, 0), np.append(center_to_point_vector, 0)
+    )
+
+    # If the z-component of the cross product is positive, it's a right turn; if negative, it's a left turn
+    turn_direction = (
+        -1 if cross_product[2] > 0 else 1
+    )  # Left turn: negative curvature, Right turn: positive curvature
+    return xc, yc, r * turn_direction
+
+
+def circle_residuals(params, points):
+    xc, yc, r = params
+    return np.sqrt((points[:, 0] - xc) ** 2 + (points[:, 1] - yc) ** 2) - r
